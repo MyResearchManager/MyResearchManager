5a39190f89e20601f454ad51b963b5b792374792
Igor Machado <igor.machado@gmail.com>
2012-07-19 14:53:42 -0300

    created sample file for mysql connection

--- site/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $host = "localhost";
-   $user = "user";
-   $pass = "pwd";
-
-   $myrmconn = mysql_connect($host, $user, $pass) or die ("Error connecting server");
-   $db = mysql_select_db("myrm", $myrmconn) or mysql_close($myrmconn) and die ("Error selecting database");
- ?>
