557147d9b1190b757db16a8ba47a389512804fd5
Igor Machado <igor.machado@gmail.com>
2012-09-13 21:28:13 -0300

    put things to remember in the area options page

--- site/area_options.php
+++ site/area_options.php
@@ -52,6 +52,41 @@ WHERE R.idResearch = RM.idResearch and RM.idUser = $id and R.idArea = A.idArea O
 </form>
 
 <br><br>
+
+<b>Things to remember</b><br>
+<ul>
+
+<?php
+      $sql = "SELECT A.`name` as aname, I.`description` as description, S.`title` as title, I.`datetime` as dt FROM
+Areas as A, Researches as R, ResearchMembers as RM, Sections as S, ImportantDates as I  WHERE
+R.idArea=A.idArea and RM.idUser='$id' and RM.idResearch = R.idResearch and 
+S.idResearch = R.idResearch and I.idSection = S.idSection and I.`datetime` > NOW() ORDER BY I.`datetime`";
+
+      $exe = mysql_query( $sql, $myrmconn) or print(mysql_error());
+      if($exe != null)
+          while($line = mysql_fetch_array($exe)) // should be while!
+          {
+              $description = $line['description'];
+              $dt          = $line['dt'];
+              $title	   = $line['title'];
+              $aname       = $line['aname'];
+
+              $event = new DateTime("$dt");
+              $today = new DateTime();
+              $days = round(abs($event->format('U') - $today->format('U')) / (60*60*24));
+
+              echo "<li>";
+              if($days == 0)
+                 echo "<font color=\"#FF0000\"><b>$aname/<i>$title</i></b> - $description (<i>Today!</i>)</font>\n";
+              else if($days == 1)
+                 echo "<font color=\"#FF0000\"><b>$aname/<i>$title</i></b> - $description (<i>Tomorrow</i>)</font>\n";
+              else
+                 echo "<b>$aname/<i>$title</i></b> - $description (<i>In $days days</i>)";
+          }
+?>
+</ul>
+
+<br><br>
 <i>Or...</i> <b>create a new research area:</b><br>
 <br>
 
