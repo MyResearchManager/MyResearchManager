5aa79ba74af0dcc0af6b77449131ab55c359092e
Igor Machado <igor.machado@gmail.com>
2013-02-26 01:38:40 -0300

    current 0.6 version database

--- database/myrm.sql
+++ database/myrm.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `DynamicCells` (
   `idDynamicCell` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `column` int(11) NOT NULL,
   `row` int(11) NOT NULL,
-  `value` varchar(20) NOT NULL,
+  `value` varchar(100) NOT NULL,
   `idDynamicTable` int(11) NOT NULL,
   PRIMARY KEY (`idDynamicCell`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
@@ -41,6 +41,7 @@ CREATE TABLE IF NOT EXISTS `DynamicTables` (
   `description` varchar(100) NOT NULL,
   `key` varchar(32) NOT NULL,
   `locked` tinyint(1) NOT NULL,
+  `lastUpdate` datetime NOT NULL,
   `idSection` int(10) unsigned NOT NULL,
   PRIMARY KEY (`idDynamicTable`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
@@ -55,9 +56,8 @@ CREATE TABLE IF NOT EXISTS `Files` (
   `idFile` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `filename` varchar(150) NOT NULL,
   `size` int(10) unsigned NOT NULL,
-  `uploadDateTime` datetime NOT NULL,
-  `uploadUser` int(11) NOT NULL,
-  `public` tinyint(1) NOT NULL,
+  `creation` datetime NOT NULL,
+  `visible` int(1) NOT NULL,
   `idSection` int(11) NOT NULL,
   PRIMARY KEY (`idFile`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
@@ -93,6 +93,19 @@ CREATE TABLE IF NOT EXISTS `Links` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `Logs`
+--
+
+CREATE TABLE IF NOT EXISTS `Logs` (
+  `idLog` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `when` datetime NOT NULL,
+  `what` varchar(255) NOT NULL,
+  PRIMARY KEY (`idLog`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `PublicationFiles`
 --
 
@@ -115,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `PublicationMembers` (
   `idUser` int(11) NOT NULL,
   `idPublication` int(11) NOT NULL,
   PRIMARY KEY (`idPublicationMember`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -126,6 +139,7 @@ CREATE TABLE IF NOT EXISTS `PublicationMembers` (
 CREATE TABLE IF NOT EXISTS `Publications` (
   `idPublication` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `date` date NOT NULL,
   `abstract` text NOT NULL,
   `year` int(11) NOT NULL,
   `journal` varchar(100) NOT NULL,
@@ -133,10 +147,10 @@ CREATE TABLE IF NOT EXISTS `Publications` (
   `bibtex` text NOT NULL,
   `filename` varchar(50) NOT NULL,
   `publicFile` tinyint(1) NOT NULL,
-  `public` tinyint(1) NOT NULL,
-  `idResearch` int(11) NOT NULL,
+  `visible` int(1) NOT NULL,
+  `idSection` int(11) NOT NULL,
   PRIMARY KEY (`idPublication`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -168,6 +182,19 @@ CREATE TABLE IF NOT EXISTS `ResearchMembers` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `SectionMembers`
+--
+
+CREATE TABLE IF NOT EXISTS `SectionMembers` (
+  `idSectionMember` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idSection` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idSectionMember`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `Sections`
 --
 
@@ -188,7 +215,10 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `idUser` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `password` varchar(32) NOT NULL,
+  `salt` char(4) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `confirmationCode` varchar(32) NOT NULL,
+  `checkedEmail` tinyint(1) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`idUser`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
@@ -223,3 +253,7 @@ CREATE TABLE IF NOT EXISTS `VCTypes` (
   `description` varchar(30) NOT NULL,
   PRIMARY KEY (`idVCType`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
